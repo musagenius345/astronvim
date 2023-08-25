e098f2f6d535cd6f7a559f41ac401c9480b7a340
musagenius345 <musabadru@gmail.com>
2023-08-25 08:48:01 +0300

    commented multi-select, used a lua version of beacon

--- plugins/beacon.lua
+++ plugins/beacon.lua
@@ -1,5 +1,18 @@
 return {
-  "danilamihailov/beacon.nvim",
+  "rainbowhxch/beacon.nvim",
   event = "VeryLazy",
-  opts = {}
+  -- opts = {}
+  config = function()
+    require('beacon').setup({
+	enable = true,
+	size = 40,
+	fade = true,
+	minimal_jump = 10,
+	show_jumps = true,
+	focus_gained = false,
+	shrink = true,
+	timeout = 500,
+	ignore_buffers = {},
+	ignore_filetypes = {},
+})  end
 }
--- plugins/multi-select.lua
+++ plugins/multi-select.lua
@@ -1,5 +1,9 @@
 return {
-  "mg979/vim-visual-multi",
-  event="VeryLazy",
-  opts={}
+  -- "mg979/vim-visual-multi",
+  -- event="VeryLazy",
+  -- disabled = true,
+  -- -- opts={},
+  -- config = function()
+  --   require("vim-visual-multi").setup()
+  -- end
 }
